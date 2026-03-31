a456e0b716737dd0116ae2300a0a0bf63de6d0ea
Cristian Perez <cperezlecaros@gmail.com>
2026-03-31 12:25:40 -0300

    Basics and nullability

---
+++ src/main/kotlin/Nullability.kt
@@ -0,0 +1,19 @@
+fun checkNullability() {
+    println("Ingresa algun dato")
+    val input = readln()
+
+    //si input es nulo se asigna el valor 0
+    val inputAsInteger = input.toIntOrNull() ?: 0
+
+    val isEven = inputAsInteger % 2 == 0
+
+    println("is it even: $isEven")
+
+    //null safety operator
+    println("Ingresa algun dato")
+    val newNum = readln()
+    val newInputasInteger = newNum.toIntOrNull()?.inc()
+
+    println("The incremented number is $newInputasInteger")
+
+}
